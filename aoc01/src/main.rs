41cb52456d9256e4dc9e38320b1de10e200b272b
Jamie Reid <jamie@jre.id.au>
2019-03-03 17:29:53 +1100

    day1: part two completed

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -2,6 +2,7 @@
 extern crate failure;
 
 use failure::ResultExt;
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
@@ -21,16 +22,36 @@ fn read_in_file(file: &str) -> Result<String, failure::Error> {
 fn main() -> Result<(), failure::Error> {
     let input;
     if let Some(file) = env::args().nth(1) {
-        println!("=> Got filename: {}", file);
+        println!("=> Got filename: {}", &file);
         input = read_in_file(&file)?;
     } else {
         bail!("Hey!, We need a filename.")
     }
 
+    // Part 1
     let freq: isize = input.lines().fold(0, |acc, line| {
         let c: isize = line.parse().unwrap(); // @Note: we're guaranteed good input, so we're just using unwrap. @Study: a better way!
         acc + c
     });
-    println!("{}", freq);
-    Ok(())
+
+    println!("Part 1: {}", freq);
+
+    // Part 2
+    let mut freq = 0;
+    let mut freqs_seen = HashSet::new();
+    freqs_seen.insert(freq);
+
+    // tried to use input.lines().cycle().map() but had no luck :(
+    loop {
+        for line in input.lines() {
+            let c: isize = line.parse().unwrap(); // @Note: we're guaranteed good input, so we're just using unwrap. @Study: a better way!
+            freq += c;
+
+            if freqs_seen.contains(&freq) {
+                println!("Part 2: {}", freq);
+                return Ok(());
+            }
+            freqs_seen.insert(freq);
+        }
+    }
 }
