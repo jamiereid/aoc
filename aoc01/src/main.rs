3efcd8effd0cac8b0855817e8508575bbc42851e
Jamie Reid <jamie@jre.id.au>
2019-03-03 16:33:50 +1100

    day1: part one completed

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,24 +1,24 @@
 #[macro_use]
 extern crate failure;
 
+use failure::ResultExt;
 use std::env;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
-use failure::ResultExt;
+use std::io::BufReader;
 
 fn read_in_file(file: &str) -> Result<String, failure::Error> {
     let of = File::open(file).context(format!("Failed to open file {}", file))?;
     let mut buf_reader = BufReader::new(of);
     let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents).context("Had trouble reading file contents to String")?;
+    buf_reader
+        .read_to_string(&mut contents)
+        .context("Had trouble reading file contents to String")?;
 
     Ok(contents)
 }
 
-
 fn main() -> Result<(), failure::Error> {
-
     let input;
     if let Some(file) = env::args().nth(1) {
         println!("=> Got filename: {}", file);
@@ -27,7 +27,10 @@ fn main() -> Result<(), failure::Error> {
         bail!("Hey!, We need a filename.")
     }
 
-    println!("{}", input);
-
+    let freq: isize = input.lines().fold(0, |acc, line| {
+        let c: isize = line.parse().unwrap(); // @Note: we're guaranteed good input, so we're just using unwrap. @Study: a better way!
+        acc + c
+    });
+    println!("{}", freq);
     Ok(())
 }
