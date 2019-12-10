5443bbf301a3fbb5043c599c2621730c20c0025e
Jamie Reid <jamie@jre.id.au>
2019-12-10 15:46:05 +1100

    slight refactors

--- 2019/aoc02/code.py
+++ 2019/aoc02/code.py
@@ -4,54 +4,73 @@
 import os
 import inspect
 
-class IntcodeProgram():
-    def __init__(self, tape):
-        self.program = tape
-        self.programlen = len(self.program)
-        self.cursor = 0
-        self.icurr = (None, None, None, None)
-       # tuple([self.program[self.cursor],
-       #                     self.program[self.cursor + 1],
-       #                     self.program[self.cursor + 2],
-       #                     self.program[self.cursor + 3]])
+class IntcodeComputer():
+    def __init__(self, instruction_set):
+        self._memory = []
+        self._instruction_set = instruction_set
+        self._maxmemsize = len(self._memory)
+        self._memptr = 0
 
+        self._current_inst = (None, None, None, None)
+
+    # @Todo: abstract memory out - it makes more sense to iterate over memory
+    #        addresses than it does to iterate over a computer
     def __iter__(self):
         return self
 
     def __next__(self):
-        if (self.cursor + 4) > self.programlen:
+        if (self._memptr + 4) > self._maxmemsize:
             raise StopIteration()
 
-        self.icurr = tuple([self.program[self.cursor],
-                            self.program[self.cursor + 1],
-                            self.program[self.cursor + 2],
-                            self.program[self.cursor + 3]])
-        self.cursor = self.cursor + 4
-        return self.icurr
+        self._current_inst = tuple([self._memory[self._memptr],
+                                    self._memory[self._memptr + 1],
+                                    self._memory[self._memptr + 2],
+                                    self._memory[self._memptr + 3]])
+        self._memptr += 4
+        return self._current_inst
 
-    def dump(self):
-        return self.program
+    def memdump(self):
+        return self._memory
 
-    def get_register(self, x):
-        return self.program[x]
+    def readaddr(self, addr):
+        return self._memory[addr]
 
-    def set_register(self, x, value):
-        self.program[x] = value
+    def wraddr(self, addr, value):
+        self._memory[addr] = value
 
-    def patch(self, patch):
-        for r, v in patch:
-            self.set_register(r, v)
+    def memload(self, tape):
+        self._memory = tape
+        self._maxmemsize = len(self._memory)
 
-    def process_instruction(self, iset, i):
-        (o, ae, be, r) = i
+    def mempatch(self, patch):
+        for addr, value in patch:
+            self.wraddr(addr, value)
 
-        func = iset.get(o, "HALT")
+    def setpointer(self, addr):
+        self._mempointer = addr
+
+    def run(self):
+        for inst in iter(self):
+            self._do_instruction(inst)
+
+    def step(self):
+        inst = tuple([self._memory[self._memptr],
+                      self._memory[self._memptr + 1],
+                      self._memory[self._memptr + 2],
+                      self._memory[self._memptr + 3]])
+        self._memptr += 4
+        self._do_instruction(inst)
+
+    def _do_instruction(self, inst):
+        (opcode, a_addr, b_addr, r_addr) = inst
+
+        func = self._instruction_set.get(opcode, "HALT")
         if func == "HALT": return
 
-        self.set_register(r, func(self.get_register(ae), self.get_register(be)))
+        self.wraddr(r_addr, func(self.readaddr(a_addr), self.readaddr(b_addr)))
 
 
-def solve_part_one(tape):
+def solve_part_one(tape, do_patch = False):
     patch = [(1, 12), (2, 2)] # r1=12, r2=2
     p1_iset = {
         1:  (lambda a, b : a + b),
@@ -59,13 +78,13 @@ def solve_part_one(tape):
         99: "HALT"
     }
 
-    prog = IntcodeProgram(tape)
-    prog.patch(patch)
+    comp = IntcodeComputer(p1_iset)
+    comp.memload(tape)
+    if do_patch:  comp.mempatch(patch)
+    comp.setpointer(0)
+    comp.run()
 
-    for instruction in iter(prog):
-        prog.process_instruction(p1_iset, instruction)
-
-    return prog.dump()
+    return comp.memdump()
 
 
 def solve_part_two(tape):
@@ -79,5 +98,5 @@ if __name__ == '__main__':
         input = input_file.read()
     tape = [int(register) for register in input.split(',')]
 
-    print(f'Part One: {solve_part_one(tape)[0]}')
+    print(f'Part One: {solve_part_one(tape, True)[0]}')
     print(f'Part Two: {solve_part_two(tape)}')
