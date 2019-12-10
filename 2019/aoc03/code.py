376b60e67ade1dc21c7e434a31ffff5b30dda1bf
Jamie Reid <jamie@jre.id.au>
2019-12-10 19:38:02 +1100

    add day 03

---
+++ 2019/aoc03/code.py
@@ -0,0 +1,25 @@
+# Advent of Code 2019 - Day X
+# Start: datetime
+
+import os
+
+
+
+def solve_part_one(intput):
+    answer = 0
+    return str(answer)
+
+
+def solve_part_two(intput):
+    answer = 0
+    return str(answer)
+
+
+if __name__ == '__main__':
+    with open((os.path.abspath(__file__).rstrip('code.py')+'input.txt'),
+              'r') as input_file:
+        input = input_file.read()
+    intput = [int(line) for line in input.splitlines()]
+
+    print(f'Part One: {solve_part_one(intput)}')
+    print(f'Part Two: {solve_part_two(intput)}')
